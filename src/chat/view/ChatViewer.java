e04a355070656d1c4f98bd792b3f05d0acfb293a
Britton Perry <britton.perry@hotmail.com>
2016-10-18 08:16:48 -0600

    Added code to length checker

--- src/chat/view/ChatViewer.java
+++ src/chat/view/ChatViewer.java
@@ -1,5 +1,9 @@
 package chat.view;
 
-public class ChatViewer {
-
+public class ChatViewer
+{
+public static void main(String [] args)
+{
+	ChatController chatAppController = new ChatController();
+	chatAppController.start();
 }
