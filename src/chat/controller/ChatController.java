343b4d2a04173411909082a1eff3beae4e3cedff
Britton Perry <britton.perry@hotmail.com>
2016-10-18 09:24:33 -0600

    There was a problem with the runner that got fixed

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -1,10 +1,27 @@
 package chat.controller;
 
+import chat.model.Chatbot;
+import chat.view.ChatViewer;
+
 public class ChatController 
 {
-
+	private Chatbot smartBot;
+	private ChatViewer display;
+	
+	public ChatController()
+	{
+		smartBot = new Chatbot("intelligent chat machine");
+		display = new ChatViewer();
+	}
+	
 	public void start()
 	{
-		
+		String response = "words";
+				
+				while(smartBot.lengthChecker(response))
+				{
+					
+					response = display.collectResponse("Whatchu wanna talk about fam?");
+				}
 	}
 }
